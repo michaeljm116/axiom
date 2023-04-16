431d9f6d55d5be795cc22c68d87309a7890e917c
michaeljm116 <michaeljm116@gmail.com>
2023-04-16 16:26:27 -0400

    render ogl cube

--- include/core/render/opengl-tutorial.h
+++ include/core/render/opengl-tutorial.h
@@ -12,11 +12,26 @@ public:
 
     void Init();
     void Render();
+    void Update();
 
 private:
     GLFWwindow* window_;
     GLuint vao_;
     GLuint program_;
+
+
+    int width_;
+    int height_;
+    float ratio_;
+    glm::mat4 m_;
+    glm::mat4 p_;
+
+    struct PerFrameData{
+        glm::mat4 mvp_;
+        int is_wireframe;
+    }per_frame_data;
+    const GLsizeiptr k_buffer_size = sizeof(PerFrameData);
+    GLuint per_frame_data_buf;
 };
 
 } // namespace axiom
