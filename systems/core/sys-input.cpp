0946385f731516fdecb21c6485e460ce3be8fa97
michaeljm116 <michaeljm116@gmail.com>
2023-04-27 05:54:54 -0400

    transfer input

---
+++ systems/core/sys-input.cpp
@@ -0,0 +1,102 @@
+#include "pch.h"
+#include "sys-input.h"
+
+namespace axiom
+{
+
+}
+
+namespace principia{
+    	Input::~Input()
+	{
+	}
+
+	void Input::init() {
+
+		//set up callbacks
+		window = Window::get().getWindow();
+		glfwSetKeyCallback(window, Input::key_callback);
+		glfwSetCharCallback(window, Input::char_callback);
+		glfwSetCursorPosCallback(window, Input::cursor_position_callback);
+		//glfwSetMouseButtonCallback(window, Input::mouse_button_callback);
+		glfwSetInputMode(window, GLFW_STICKY_KEYS, true);
+		glfwSetScrollCallback(window, Input::scroll_callback);
+		glfwSetJoystickCallback(Input::joystick_callback);
+		
+		for (int i = 0; i < 16; ++i) {
+			if (glfwJoystickPresent(i)) {
+				int axesCount;
+				const float* axes = glfwGetJoystickAxes(i, &axesCount);
+
+				int buttonCount;
+				const unsigned char* buttons = glfwGetJoystickButtons(i, &buttonCount);
+
+				const char* name = glfwGetJoystickName(i);
+				const char* game_pad_name = "NOT a Gamepad";
+				bool is_game_pad = false;
+				GLFWgamepadstate state;
+				if (glfwJoystickIsGamepad(i)) {
+					is_game_pad = true;
+					hasGamepad = true;
+					game_pad_name = glfwGetGamepadName(i);
+					glfwGetGamepadState(i, &state);
+				}
+				//printf("%s: \t Axes:%f \t buttons:%c", name, axes, buttons);
+				printf("\n%u is a %s aka %s it has %u Axes and %u buttons\n", i, name, game_pad_name, axesCount, buttonCount);
+			}
+		}
+
+
+
+
+		/*
+		Q, E, R, F, V
+		space, tab, shift
+		1, 2, 3, 4, 5
+
+		awdsdqdeqdewfvafadfvaddawda
+
+		*/
+
+		//set up mouse
+
+		double x, y;
+		glfwGetCursorPos(window, &x, &y);
+		mouse.x = x;
+		mouse.prevX = x;
+		mouse.y = y;
+		mouse.prevY = y;
+
+		//set up timer
+		static auto startTime = std::chrono::high_resolution_clock::now();
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		time = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime).count() / 1000.0f;
+		deltaTime = time;
+		showFPS = true;
+	}
+
+	void Input::KeyDirection(int key, bool pressed) {
+
+		//for (int i = 0; i < 6; ++i) {
+		//	if (key == Keys[i].key) { 
+		//		Keys[i].update(pressed);
+		//		return;
+		//	}
+		//}
+	}
+
+	void Input::update()
+	{
+		static auto startTime = std::chrono::high_resolution_clock::now();
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		float newTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime).count() / 1000.0f;
+		deltaTime = abs(time - newTime);
+		time = newTime;
+
+		int left_mb = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+		int right_mb = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
+		mouse.updateButton(GLFW_MOUSE_BUTTON_LEFT, left_mb);
+		mouse.updateButton(GLFW_MOUSE_BUTTON_RIGHT, right_mb);
+		
+	}
+}
---
+++ systems/core/sys-input.h
@@ -0,0 +1,136 @@
+#pragma once
+#include "cmp-input.h"
+#include <flecs-world.h>
+
+namespace axiom
+{
+    class Sys_Input
+    {
+        Sys_Input();
+        ~Sys_Input();
+
+        
+    }
+}
+
+
+namespace Principia {
+
+#define pINPUT Input::get()
+
+	class Input
+	{
+	public:
+
+		static Input& get() {
+			static Input instance;
+			return instance;
+		}
+	private:
+		Input() {};
+		~Input();
+
+		GLFWwindow* window;
+
+		void KeyDirection(int key, bool pressed);
+
+		static void key_callback(GLFWwindow * window, int key, int scancode, int action, int mods)
+		{
+			pINPUT.keys[key] = action;
+			switch (action)
+			{
+			case GLFW_PRESS:
+				pINPUT.pressed = true;
+				break;
+			case GLFW_REPEAT:
+				pINPUT.pressed = true;
+				break;
+			case GLFW_RELEASE:
+				pINPUT.pressed = false;
+				break;
+			default:
+				break;
+			}
+
+#ifdef UIIZON
+			ImGuiIO& io = ImGui::GetIO();
+			if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+				io.KeysDown[key] = true;
+			}
+			else if (action == GLFW_RELEASE) {
+				io.KeysDown[key] = false;
+			}
+#endif // 
+
+
+		}
+
+		static void char_callback(GLFWwindow*, unsigned int c)
+		{
+#ifdef UIIZON
+			ImGuiIO& io = ImGui::GetIO();
+			io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
+			io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
+			io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
+			if (c > 0 && c < 0x10000)
+				io.AddInputCharacter((unsigned short)c);
+#endif // UIIZON
+		}
+
+		static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
+			pINPUT.mouse.updatePosition((int)xpos, (int)ypos);
+		}
+
+
+		//static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+		//	/*if (action == GLFW_PRESS || action == GLFW_REPEAT)
+		//		INPUT.mouse.updateButton(button, true);
+		//	else
+		//		INPUT.mouse.updateButton(button, false);*/
+		//	INPUT.mouse.buttons[button] = action;
+		//	//INPUT.mouse.updateButton(button, (bool)action);
+		//}
+
+		static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+			pINPUT.mouse.updateScroll(yoffset);
+		}
+
+		static void joystick_callback(int jid, int event) {
+			if (event == GLFW_CONNECTED)
+			{
+				// The joystick was connected
+			}
+			else if (event == GLFW_DISCONNECTED)
+			{
+				// The joystick was disconnected
+			}
+		}
+
+
+	public:
+		void init();
+		void update();
+		void updateAxis(int key, bool pressed);
+
+		//bool up, down, left, right;
+		//bool pressed, held, released;
+		Mouse mouse;
+		int keys[348];
+		int pad[8][14];
+
+		float deltaTime;
+		float time;
+		float renderTime;
+		bool pressed;
+		bool hold;
+		bool showFPS;
+		bool maximized = false;
+		bool displayUI = false;
+		bool playToggled = false;
+		bool playMode = false;
+
+		bool hasGamepad = false;
+	};
+
+#endif // !INPUT_H
+}
