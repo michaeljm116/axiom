b9e0c12d2ab90d893fdc461e085e483dfc250b64
michaeljm116 <michaeljm116@gmail.com>
2023-05-17 07:39:25 -0400

    transfer

--- components/render/cmp-vulkan.h
+++ components/render/cmp-vulkan.h
@@ -1,18 +1,21 @@
 #pragma once
 #include <volk.h>
 #include <vector>
+#include "vulkan/vulkan-base.hpp"
 namespace axiom
 {
     namespace render
     {
         namespace vulkan
         {
+            /** @brief Device Queues */
             struct Queues{
                 VkQueue graphics;
                 VkQueue present;
                 VkQueue compute;
             };
 
+             /** @brief Swapchain*/
             struct SwapChain{
                 struct SupportDetails
                 {
@@ -29,17 +32,33 @@ namespace axiom
                 std::vector<VkFramebuffer> frame_buffers;
             };
 
+            /** @brief Command Buffers and pool*/
             struct Command{
                 std::vector<VkCommandBuffer> buffers;
                 VkCommandPool pool;
             };
 
+            /** @brief Semaphores*/
             struct Semaphores{
               VkSemaphore image_available;
               VkSemaphore render_finished;  
-            }
+            };
 
+            /** @brief Vulkan Device Info*/
+            struct DeviceInfo{
+                VulkanDevice* device;
+                VkQueue copy_queue;
+                VkRenderPass render_pass;
+                std::vector<VkFramebuffer> frame_buffers;
+                VkFormat color_format;
+                VkFormat depth_format;
+                uint32_t width;
+                uint32_t heigt;
+                
+            };
         }
+
+
         struct Cmp_Vulkan{
 
         }
